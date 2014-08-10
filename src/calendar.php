23c4811791289f538013a27cca0d3f26d6616921
Ben Plummer <ben@benplummer.co.uk>
2014-08-10 15:18:16 +0100

    Initial commit.

---
+++ src/calendar.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Plummer\Calendar;
+
+class Calendar
+{
+
+}
---
+++ tests/plummer/calendar/CalendarTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Plummer\Calendar;
+
+class CalendarTest extends \PHPUnit_Framework_TestCase
+{
+	public function setUp(){}
+
+	public function testCalendar()
+	{
+		$this->assertTrue(true);
+	}
+}
