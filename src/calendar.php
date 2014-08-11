61aa0c60ab37c00bd82344c72cdaffd68c4e0504
Ben Plummer <ben@benplummer.co.uk>
2014-08-11 21:06:30 +0100

    Allow recurrence types to be added to a calendar to allow generation.

--- src/calendar.php
+++ src/calendar.php
@@ -6,6 +6,8 @@ class Calendar
 {
 	protected $events;
 
+	protected $recurrenceTypes;
+
 	public function addEvents(array $events)
 	{
 		foreach($events as $event) {
@@ -16,5 +18,17 @@ class Calendar
 	public function addEvent(Event $event)
 	{
 		$this->events[] = $event;
+	}
+
+	public function addRecurrenceTypes(array $recurrenceTypes)
+	{
+		foreach($recurrenceTypes as $recurrenceType) {
+			$this->addRecurrenceType($recurrenceType);
+		}
+	}
+
+	public function addRecurrenceType(RecurrenceInterface $recurrenceType)
+	{
+		$this->recurrenceTypes[] = $recurrenceType;
 	}
 }
