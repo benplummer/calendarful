d62fbb154345974f5bc63e6867d6554b9d11a733
Ben Plummer <ben@benplummer.co.uk>
2014-08-13 21:58:48 +0100

    Added name property and constructors to Calendar class.

--- src/calendar.php
+++ src/calendar.php
@@ -4,9 +4,23 @@ namespace Plummer\Calendar;
 
 class Calendar
 {
+	protected $name;
+
 	protected $events;
 
 	protected $recurrenceTypes;
+
+	protected function __construct($name, array $events, array $recurrenceTypes)
+	{
+		$this->name = $name;
+		$this->addEvents($events);
+		$this->addRecurrenceTypes($recurrenceTypes);
+	}
+
+	public static function make($name, array $events, array $recurrenceTypes)
+	{
+		return new static($name, $events, $recurrenceTypes);
+	}
 
 	public function addEvents(array $events)
 	{
