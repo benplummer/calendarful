67059786c293c656517889c4f8b3597b7a5f72b5
Ben Plummer <ben@benplummer.co.uk>
2014-08-13 21:28:16 +0100

    Initial Event class properties and constructor.

--- src/Event.php
+++ src/Event.php
@@ -4,7 +4,31 @@ namespace Plummer\Calendar;
 
 class Event
 {
+	protected $name;
+
+	protected $dateStart;
+
+	protected $dateEnd;
+
+	protected $occurrenceDateStart;
+
+	protected $parent;
+
 	protected $recurrenceType;
+	
+	protected function __construct($name, $dateStart, $dateEnd, $parent = null, $occurrenceDateStart = null)
+	{
+		$this->name = $name;
+		$this->dateStart = $dateStart;
+		$this->dateEnd = $dateEnd;
+		$this->parent = $parent;
+		$this->occurrenceDateStart = $occurrenceDateStart;
+	}
+
+	public static function make($name, $dateStart, $dateEnd, $parent = null, $occurrenceDateStart = null)
+	{
+		return new static($name, $dateStart, $dateEnd, $parent, $occurrenceDateStart);
+	}
 
 	public function setRecurrenceType(RecurrenceInterface $recurrenceType)
 	{
