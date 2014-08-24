586786cbbe644564b42625de823a4acaba94fd4b
Ben Plummer <ben@benplummer.co.uk>
2014-08-24 18:36:49 +0100

    Move previous adaptable interface methods into actual Event interface.

--- src/EventInterface.php
+++ src/EventInterface.php
@@ -4,5 +4,13 @@ namespace Plummer\Calendar;
 
 interface EventInterface
 {
+	public function getName();
 
+	public function getStartDate();
+
+	public function getEndDate();
+
+	public function getOccurrenceDate();
+
+	public function getParent();
 }
