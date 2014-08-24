466592001784de26c0b4758036c846ef98b2e91c
Ben Plummer <ben@benplummer.co.uk>
2014-08-24 18:37:24 +0100

    Update implemented interface methods and properties on Event class.

--- src/Event.php
+++ src/Event.php
@@ -8,29 +8,56 @@ class Event implements EventInterface
 
 	protected $name;
 
-	protected $dateStart;
+	protected $startDate;
 
-	protected $dateEnd;
+	protected $endDate;
 
-	protected $occurrenceDateStart;
+	protected $occurrenceDate;
 
 	protected $calendar;
+
+	protected $parent;
 	
-	protected function __construct($uniqueId, $name, $dateStart, $dateEnd)
+	protected function __construct($uniqueId, $name, $startDate, $endDate)
 	{
 		$this->uniqueId = $uniqueId;
 		$this->name = $name;
-		$this->dateStart = $dateStart;
-		$this->dateEnd = $dateEnd;
+		$this->startDate = $startDate;
+		$this->endDate = $endDate;
 	}
 
-	public static function make($name, $dateStart, $dateEnd, $occurrenceDateStart = null)
+	public static function make($name, $startDate, $endDate, $occurrenceDate = null)
 	{
-		return new static($name, $dateStart, $dateEnd, $occurrenceDateStart);
+		return new static($name, $startDate, $endDate, $occurrenceDate);
 	}
 
 	public function setCalendar(Calendar &$calendar)
 	{
 		$this->calendar = $calendar;
+	}
+
+	public function getName()
+	{
+		return $this->name;
+	}
+
+	public function getStartDate()
+	{
+		return $this->startDate;
+	}
+
+	public function getEndDate()
+	{
+		return $this->endDate;
+	}
+
+	public function getParent()
+	{
+		return $this->parent;
+	}
+
+	public function getOccurrenceDate()
+	{
+		return $this->occurrenceDate;
 	}
 }
