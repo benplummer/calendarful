445332f59a445adc358732ce3d5d55f118a6545e
Ben Plummer <ben@benplummer.co.uk>
2014-11-02 19:59:55 +0000

    Simplifying the registry interface to one method.

--- src/RegistryInterface.php
+++ src/RegistryInterface.php
@@ -4,30 +4,5 @@ namespace Plummer\Calendarful;
 
 interface RegistryInterface
 {
-	/**
-	 * sets a value
-	 *
-	 * @param string $key
-	 * @param mixed $value
-	 *
-	 * @static
-	 * @return void
-	 */
-	public function set($key, $value);
-
-	/**
-	 * gets a value from the registry
-	 *
-	 * @param string $key
-	 *
-	 * @static
-	 * @return mixed
-	 */
-	public function get($key);
-
-	public function getAll();
-
-	public function getFiltered();
-
-	public function hasFilters();
+	public function get(Array $filters = []);
 }
