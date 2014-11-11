c858ff9c13b5cc185268e1e47e546f7a396acdba
Ben Plummer <ben@benplummer.co.uk>
2014-11-11 19:52:24 +0000

    Change Calendar Factory to actually create a calendar.

--- src/Calendar/CalendarFactory.php
+++ src/Calendar/CalendarFactory.php
@@ -7,24 +7,9 @@ use Plummer\Calendarful\Recurrence\RecurrenceFactoryInterface;
 
 class CalendarFactory implements CalendarFactoryInterface
 {
-	private $calendars;
-
-	public function addCalendars(Array $calendars)
-	{
-		foreach($calendars as $calendar) {
-			$this->addCalendar($calendar);
-		}
-	}
-
-	public function addCalendar(CalendarInterface $calendar) {
-		$this->calendars[$calendar->getName()] = $calendar;
-	}
-
 	public function createCalendar($name, RecurrenceFactoryInterface $recurrenceFactory = null)
 	{
-		$calendar = $this->calendars[$name];
-
-		$calendar->addRecurrenceTypes($recurrenceFactory);
+		$calendar = new Calendar($name, $recurrenceFactory);
 
 		return $calendar;
 	}
