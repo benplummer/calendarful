a491f0fe1d8662c4d3033a1548e66b2dc7d26b27
Ben Plummer <ben@benplummer.co.uk>
2014-11-17 20:03:08 +0000

    Clean up calendar functionality.

--- src/Calendar/Calendar.php
+++ src/Calendar/Calendar.php
@@ -22,29 +22,17 @@ class Calendar implements CalendarInterface, \IteratorAggregate
 			throw new \Exception('This calendar needs to be populated with events.');
 		}
 
-		return $this->events;
+		return new \ArrayIterator($this->events);
 	}
 
 	public function populate(RegistryInterface $eventsRegistry, \DateTime $fromDate, \DateTime $toDate, $limit = null)
 	{
 		$filters = [
-			'fromDate' => $fromDate->format('Y-m-d'),
-			'toDate' => $toDate->format('Y-m-d')
+			'fromDate' => $fromDate,
+			'toDate' => $toDate
 		];
 
-		// Filter events within the date range
-		$this->events = array_filter($eventsRegistry->get($filters), function($event) use ($fromDate, $toDate) {
-			if($event->getStartDateFull() <= $toDate && $event->getEndDate() >= $fromDate->format('Y-m-d')) {
-				return true;
-			}
-			else if($event->getRecurrenceType()) {
-				if($event->getRecurrenceUntil() === null || $event->getRecurrenceUntil() >= $fromDate->format('Y-m-d')) {
-					return true;
-				}
-			}
-
-			return false;
-		});
+		$this->events = $eventsRegistry->get($filters);
 
 		if($this->recurrenceFactory) {
 			foreach($this->recurrenceFactory->getRecurrenceTypes() as $label => $recurrence) {
@@ -54,42 +42,6 @@ class Calendar implements CalendarInterface, \IteratorAggregate
 			}
 		}
 
-		// Remove recurring events that do not occur within the date range
-		$this->events = array_filter($this->events, function($event) use ($fromDate, $toDate) {
-			if(! $event->getRecurrenceType()) {
-				return true;
-			}
-			else if($event->getStartDateFull() <= $toDate->format('Y-m-d') && $event->getEndDate() >= $fromDate->format('Y-m-d')) {
-				return true;
-			}
-			else {
-				return false;
-			}
-		});
-
-		//Restructure events under their relevant day
-		foreach($this->events as $id_and_date => $event) {
-
-			list($id, $date) = explode('.', $id_and_date);
-
-			$events[$date][] = $event;
-		}
-
-		foreach($events as $date => &$event) {
-			usort($event, function($e1, $e2) use ($date) {
-
-				if($e1->getStartDateFull() == $e2->getStartDateFull()) {
-					return 0;
-				}
-
-				return $e1->getStartDateFull() < $e2->getStartDateFull() ? -1 : 1;
-			});
-		}
-
-		ksort($events);
-
-		$this->events = $events;
-
-		return $this->events;
+		$this->events = array_values($this->events);
 	}
 }
