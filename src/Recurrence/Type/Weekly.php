33f66261b1f1d31f83c4431789f631902561d902
Ben Plummer <ben@benplummer.co.uk>
2014-11-22 20:31:06 +0000

    Weekly recurrence type implementation.

---
+++ src/Recurrence/Type/Weekly.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Plummer\Calendarful\Recurrence\Type;
+
+use Plummer\Calendarful\Recurrence\RecurrenceInterface;
+
+class Weekly implements RecurrenceInterface
+{
+	protected $label = 'weekly';
+
+	protected $limit = '+5 year';
+
+	public function getLabel()
+	{
+		return $this->label;
+	}
+
+	public function getLimit()
+	{
+		return $this->limit;
+	}
+
+	public function generateOccurrences(Array $events, \DateTime $fromDate, \DateTime $toDate, $limit = null)
+	{
+		$return = [];
+
+		$weeklyEvents = array_filter($events, function ($event) {
+			return $event->getRecurrenceType() === $this->getLabel();
+		});
+
+		foreach ($weeklyEvents as $weeklyEvent) {
+
+			$startMarker = $fromDate > new \DateTime($weeklyEvent->getStartDate())
+				? $fromDate
+				: new \DateTime($weeklyEvent->getStartDate());
+
+			$endMarker = $weeklyEvent->getRecurrenceUntil()
+				? min(new \DateTime($weeklyEvent->getRecurrenceUntil()), $toDate)
+				: $toDate;
+
+			// The DatePeriod class does not actually include the end date so you have to increment it first
+			$endMarker->modify('+1 day');
+
+			$dateInterval = new \DateInterval('P1W');
+			$datePeriod = new \DatePeriod($startMarker, $dateInterval, $endMarker);
+
+			foreach($datePeriod as $date) {
+				$newWeeklyEvent = clone($weeklyEvent);
+				$newStartDate = $date;
+				$duration = $newWeeklyEvent->getDuration();
+
+				$newWeeklyEvent->setStartDate($newStartDate);
+				$newStartDate->add($duration);
+				$newWeeklyEvent->setEndDate($newStartDate);
+				$newWeeklyEvent->setRecurrenceType();
+
+				$return[] = $newWeeklyEvent;
+			}
+		}
+
+		return $return;
+	}
+}
