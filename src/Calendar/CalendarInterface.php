69609183cb9594fbc1cd9603efa1988e4bc68a45
Ben Plummer <ben@benplummer.co.uk>
2014-12-01 18:25:40 +0000

    Change populate method to allow limit to be null and take an extra array of filters.

--- src/Calendar/CalendarInterface.php
+++ src/Calendar/CalendarInterface.php
@@ -6,7 +6,7 @@ use Plummer\Calendarful\RegistryInterface;
 
 interface CalendarInterface
 {
-	public function populate(RegistryInterface $eventsRegistry, \DateTime $fromDate, \DateTime $toDate, $limit);
+	public function populate(RegistryInterface $eventsRegistry, \DateTime $fromDate, \DateTime $toDate, $limit = null, Array $extraFilters = array());
 
 	public function sort();
 }
