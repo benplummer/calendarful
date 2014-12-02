9844364e5fae2f71004406516186671949859ce3
Ben Plummer <ben@benplummer.co.uk>
2014-12-02 16:14:13 +0000

    Add more calendar event date range tests.

--- tests/Calendar/CalendarTest.php
+++ tests/Calendar/CalendarTest.php
@@ -12,7 +12,7 @@ class CalendarTest extends \PHPUnit_Framework_TestCase
 		m::close();
 	}
 
-	public function testCalendarOneEventWithinRange()
+	public function testCalendarEventWithinDateRange()
 	{
 		$calendar = new Calendar();
 
@@ -35,6 +35,80 @@ class CalendarTest extends \PHPUnit_Framework_TestCase
 
 		foreach($calendar as $event) {
 			$this->assertEquals('2014-12-02 00:00:00', $event->getStartDate());
+		}
+	}
+
+	public function testCalendarEventOutOfDateRange()
+	{
+		$calendar = new Calendar();
+
+		$eventRegistry = m::mock('\Plummer\Calendarful\RegistryInterface');
+		$eventRegistry->shouldReceive('get')
+			->once()
+			->andReturn(
+				[
+					new MockEvent(
+						1,
+						'2014-12-05 00:00:00',
+						'2014-12-05 00:00:00'
+					)
+				]
+			);
+
+		$calendar->populate($eventRegistry, new \DateTime('2014-12-01 00:00:00'), new \DateTime('2014-12-03 00:00:00'));
+
+		$this->assertEquals(0, $calendar->count());
+	}
+
+	public function testCalendarEventStartDateWithinDateRange()
+	{
+		$calendar = new Calendar();
+
+		$eventRegistry = m::mock('\Plummer\Calendarful\RegistryInterface');
+		$eventRegistry->shouldReceive('get')
+			->once()
+			->andReturn(
+				[
+					new MockEvent(
+						1,
+						'2014-12-02 00:00:00',
+						'2014-12-04 00:00:00'
+					)
+				]
+			);
+
+		$calendar->populate($eventRegistry, new \DateTime('2014-12-01 00:00:00'), new \DateTime('2014-12-03 00:00:00'));
+
+		$this->assertEquals(1, $calendar->count());
+
+		foreach($calendar as $event) {
+			$this->assertEquals('2014-12-02 00:00:00', $event->getStartDate());
+		}
+	}
+
+	public function testCalendarEventEndDateWithinDateRange()
+	{
+		$calendar = new Calendar();
+
+		$eventRegistry = m::mock('\Plummer\Calendarful\RegistryInterface');
+		$eventRegistry->shouldReceive('get')
+			->once()
+			->andReturn(
+				[
+					new MockEvent(
+						1,
+						'2014-11-30 00:00:00',
+						'2014-12-02 00:00:00'
+					)
+				]
+			);
+
+		$calendar->populate($eventRegistry, new \DateTime('2014-12-01 00:00:00'), new \DateTime('2014-12-03 00:00:00'));
+
+		$this->assertEquals(1, $calendar->count());
+
+		foreach($calendar as $event) {
+			$this->assertEquals('2014-11-30 00:00:00', $event->getStartDate());
 		}
 	}
 }
