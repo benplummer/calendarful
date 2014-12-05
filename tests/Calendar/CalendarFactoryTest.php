0ebfce241e3483bf3f1de0efc4275bb35f394513
Ben Plummer <ben@benplummer.co.uk>
2014-12-05 18:21:43 +0000

    Tests for the calendar factory.

---
+++ tests/Calendar/CalendarFactoryTest.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace Plummer\Calendarful\Calendar;
+
+use \Mockery as m;
+
+class CalendarFactoryTest extends \PHPUnit_Framework_TestCase
+{
+	public function tearDown()
+	{
+		m::close();
+	}
+
+	/**
+	 * @expectedException InvalidArgumentException
+	 */
+	public function testCalendarTypeClassDoesNotExist()
+	{
+		$calendarFactory = new CalendarFactory();
+
+		$calendarFactory->addCalendarType('test', 'ThisIsNotAValidFileOrFilePath');
+	}
+
+	/**
+	 * @expectedException InvalidArgumentException
+	 */
+	public function testCalendarTypeClassPathNotCalendarInterfaceImplementation()
+	{
+		$calendarFactory = new CalendarFactory();
+
+		$calendarFactory->addCalendarType('test', 'Plummer\Calendarful\Mocks\MockEvent');
+	}
+
+	/**
+	 * @expectedException InvalidArgumentException
+	 */
+	public function testCalendarTypeClassNotCalendarInterfaceImplementation()
+	{
+		$calendarFactory = new CalendarFactory();
+
+		$calendarFactory->addCalendarType('test', new \stdClass());
+	}
+
+	public function testValidCalendarTypeClassPath()
+	{
+		$calendarFactory = new CalendarFactory();
+
+		$calendarFactory->addCalendarType('test', 'Plummer\Calendarful\Calendar\Calendar');
+
+		$this->assertEquals(1, count($calendarFactory->getCalendarTypes()));
+	}
+
+	public function testValidCalendarTypeClass()
+	{
+		$calendarFactory = new CalendarFactory();
+
+		$calendarFactory->addCalendarType('test', new Calendar());
+
+		$this->assertEquals(1, count($calendarFactory->getCalendarTypes()));
+	}
+
+	/**
+	 * @expectedException OutOfBoundsException
+	 */
+	public function testNonExistentCalendarTypeClassRetrieval()
+	{
+		$calendarFactory = new CalendarFactory();
+
+		$calendar = $calendarFactory->createCalendar('test');
+	}
+
+	public function testValidCalendarTypeClassRetrieval()
+	{
+		$calendarFactory = new CalendarFactory();
+
+		$calendarFactory->addCalendarType('test', new Calendar());
+
+		$this->assertInstanceOf('Plummer\Calendarful\Calendar\CalendarInterface', $calendarFactory->createCalendar('test'));
+	}
+}
