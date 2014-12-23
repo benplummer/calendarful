0f7f3d81626af8591d87187da32cd57863f31289
Ben Plummer <ben@benplummer.co.uk>
2014-12-23 14:46:37 +0000

    Factory interfaces documentation.

--- src/Calendar/CalendarFactoryInterface.php
+++ src/Calendar/CalendarFactoryInterface.php
@@ -2,9 +2,31 @@
 
 namespace Plummer\Calendarful\Calendar;
 
+/**
+ * Interface CalendarFactoryInterface
+ *
+ * An interface for different calendar factories specifying they need to
+ * provide implementations for getting the type/s.
+ *
+ * @package Plummer\Calendarful
+ * @abstract
+ */
 interface CalendarFactoryInterface
 {
+	/**
+	 * Instantiates a calendar and returns it.
+	 *
+	 * @param $type
+	 * @return mixed
+	 * @abstract
+	 */
 	public function createCalendar($type);
 
+	/**
+	 * Get all of the calendar types from the factory.
+	 *
+	 * @return mixed
+	 * @abstract
+	 */
 	public function getCalendarTypes();
 }
--- src/Recurrence/RecurrenceFactoryInterface.php
+++ src/Recurrence/RecurrenceFactoryInterface.php
@@ -2,9 +2,31 @@
 
 namespace Plummer\Calendarful\Recurrence;
 
+/**
+ * Interface RecurrenceFactoryInterface
+ *
+ * An interface for different recurrence factories specifying they need to
+ * provide implementations for getting the type/s.
+ *
+ * @package Plummer\Calendarful
+ * @abstract
+ */
 interface RecurrenceFactoryInterface
 {
+	/**
+	 * Instantiates a recurrence type and returns it.
+	 *
+	 * @param $type
+	 * @return mixed
+	 * @abstract
+	 */
 	public function createRecurrenceType($type);
 
+	/**
+	 * Get all of the recurrence types from the factory.
+	 *
+	 * @return mixed
+	 * @abstract
+	 */
 	public function getRecurrenceTypes();
 }
