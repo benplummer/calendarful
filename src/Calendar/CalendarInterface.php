f0c7c19231f5482c1b994de89ec18213430a2207
Ben Plummer <ben@benplummer.co.uk>
2014-12-23 16:46:23 +0000

    Documentation for the calendar, event and recurrence interfaces.

--- src/Calendar/CalendarInterface.php
+++ src/Calendar/CalendarInterface.php
@@ -4,9 +4,33 @@ namespace Plummer\Calendarful\Calendar;
 
 use Plummer\Calendarful\RegistryInterface;
 
+/**
+ * Interface CalendarInterface
+ *
+ * A common interface for calendars to use.
+ *
+ * @package Plummer\Calendarful
+ * @abstract
+ */
 interface CalendarInterface
 {
+	/**
+	 * Populate the calendar with events persisted from the event registry.
+	 *
+	 * @param RegistryInterface 	$eventsRegistry
+	 * @param \DateTime 			$fromDate
+	 * @param \DateTime 			$toDate
+	 * @param integer|null 			$limit
+	 * @param array 				$extraFilters
+	 * @return mixed
+	 * @abstract
+	 */
 	public function populate(RegistryInterface $eventsRegistry, \DateTime $fromDate, \DateTime $toDate, $limit = null, Array $extraFilters = array());
 
+	/**
+	 * Sort the events that the calendar contains.
+	 *
+	 * @return mixed
+	 */
 	public function sort();
 }
--- src/Recurrence/RecurrenceInterface.php
+++ src/Recurrence/RecurrenceInterface.php
@@ -2,11 +2,43 @@
 
 namespace Plummer\Calendarful\Recurrence;
 
+/**
+ * Interface RecurrenceInterface
+ *
+ * A common interface for recurrence types.
+ *
+ * @package Plummer\Calendarful\Recurrence
+ * @abstract
+ */
 interface RecurrenceInterface
 {
+	/**
+	 * Get the label of the recurrence type.
+	 *
+	 * This usually matches up with the recurrence type property of events.
+	 *
+	 * @return mixed
+	 * @abstract
+	 */
 	public function getLabel();
 
+	/**
+	 * Get the maximum limit of event occurrences that the recurrence type should return.
+	 *
+	 * @return mixed
+	 * @abstract
+	 */
 	public function getLimit();
 
+	/**
+	 * Generate the occurrences for recurring events of the relevant type.
+	 *
+	 * @param array 		$events
+	 * @param \DateTime 	$fromDate
+	 * @param \DateTime 	$toDate
+	 * @param integer|null 	$limit
+	 * @return mixed
+	 * @abstract
+	 */
 	public function generateOccurrences(Array $events, \DateTime $fromDate, \DateTime $toDate, $limit = null);
 }
