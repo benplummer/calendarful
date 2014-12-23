a0114eb9902627a58e2b3532f03255a49da26139
Ben Plummer <ben@benplummer.co.uk>
2014-12-23 15:07:12 +0000

    Registry interface documentation.

--- src/RegistryInterface.php
+++ src/RegistryInterface.php
@@ -2,7 +2,22 @@
 
 namespace Plummer\Calendarful;
 
+/**
+ * Interface RegistryInterface
+ *
+ * Provides an interface for registry implementations that allow the
+ * persistence of data.
+ *
+ * @package Plummer\Calendarful
+ * @abstract
+ */
 interface RegistryInterface
 {
+	/**
+	 * Gets data and allows the passing of filters if desired.
+	 *
+	 * @param array $filters
+	 * @return mixed
+	 */
 	public function get(Array $filters = array());
 }
