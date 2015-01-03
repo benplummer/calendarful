27a5bd1475788643f3aba669078ec06de946c825
Ben Plummer <ben@benplummer.co.uk>
2015-01-03 17:04:28 +0000

    Introduce a recurrent event interface that extends the event interface to abide by the interface segregation principle.
    Conflicts:
    	src/Event/EventInterface.php

---
+++ src/Event/RecurrentEventInterface.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Plummer\Calendarful\Event;
+
+/**
+ * Interface RecurrentEventInterface
+ *
+ * A common interface for recurrent events.
+ *
+ * Any event objects to be used within a calendar implementation must
+ * implement this interface and consist of properties related to the
+ * methods below.
+ *
+ * @package Plummer\Calendarful
+ * @abstract
+ */
+interface RecurrentEventInterface extends EventInterface
+{
+	/**
+	 * Get the id of the parent of the event.
+	 *
+	 * An event will tend to have a parent when it has overridden an occurrence of
+	 * the parent event that does recur.
+	 *
+	 * @return mixed
+	 * @abstract
+	 */
+	public function getParentId();
+
+	/**
+	 * Get the occurrence date of the event.
+	 *
+	 * When an occurrence of a recurring event is overridden, the date of that occurrence
+	 * should be the occurrence date property value of the new event that is created in its
+	 * place. When the start date of the parent recurring event is updated, the occurrence
+	 * date of the overriding event should also be updated.
+	 *
+	 * @return mixed
+	 * @abstract
+	 */
+	public function getOccurrenceDate();
+
+	/**
+	 * Get the recurrence type of the event.
+	 *
+	 * This is usually a string that matches up to the label of a recurrence type.
+	 *
+	 * @return mixed
+	 * @abstract
+	 */
+	public function getRecurrenceType();
+
+	/**
+	 * Set the recurrence type of the event.
+	 *
+	 * @param  null  $type
+	 * @return mixed
+	 * @abstract
+	 */
+	public function setRecurrenceType($type = null);
+
+	/**
+	 * Get the until date of the event.
+	 *
+	 * @return mixed
+	 * @abstract
+	 */
+	public function getRecurrenceUntil();
+}
