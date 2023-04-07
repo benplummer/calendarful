2e1259a2a1f1f2f4c9254779c87402264623e195
Ben Plummer <me@benplummer.uk>
2023-04-07 11:45:37 +0100

    Initial EventOccurrenceInterface and implementation.

---
+++ src/Event/Recurrence/EventOccurrence.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Plummer\Calendarful\Event\Recurrence;
+
+use DateInterval;
+use DateTimeImmutable;
+use Plummer\Calendarful\Event\EventIdInterface;
+
+class EventOccurrence implements EventOccurrenceInterface
+{
+    private function __construct(
+        private readonly EventIdInterface $id,
+        private readonly DateTimeImmutable $startDate,
+        private readonly DateTimeImmutable $endDate,
+    ) {
+    }
+
+    public static function create(
+        EventIdInterface $id,
+        DateTimeImmutable $startDate,
+        DateTimeImmutable $endDate,
+    ): EventOccurrenceInterface {
+        return new self(
+            $id,
+            $startDate,
+            $endDate,
+        );
+    }
+
+    public function id(): EventIdInterface
+    {
+        return $this->id;
+    }
+
+    public function startDate(): DateTimeImmutable
+    {
+        return $this->startDate;
+    }
+
+    public function endDate(): DateTimeImmutable
+    {
+        return $this->endDate;
+    }
+
+    public function duration(): DateInterval
+    {
+        return $this->startDate->diff($this->endDate);
+    }
+}
---
+++ src/Event/Recurrence/EventOccurrenceInterface.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Plummer\Calendarful\Event\Recurrence;
+
+use DateTimeImmutable;
+use Plummer\Calendarful\Event\EventIdInterface;
+use Plummer\Calendarful\Event\EventInterface;
+
+interface EventOccurrenceInterface extends EventInterface
+{
+    public static function create(
+        EventIdInterface $id,
+        DateTimeImmutable $startDate,
+        DateTimeImmutable $endDate,
+    ): self;
+}
