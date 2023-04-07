3fda6a6e9872ad91f39e7237a7f5238c93c63a0d
Ben Plummer <me@benplummer.uk>
2023-04-07 11:40:25 +0100

    Tests for an EventOccurrence.

---
+++ src/Event/Recurrence/EventOccurrenceTest.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Plummer\Calendarful\Event\Recurrence;
+
+use DateInterval;
+use DateTimeImmutable;
+use PHPUnit\Framework\TestCase;
+use Plummer\Calendarful\TestUtilities\EventIdFake;
+
+class EventOccurrenceTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function it_should_allow_its_properties_to_be_accessed(): void
+    {
+        $eventId = new EventIdFake('1');
+        $startDate = new DateTimeImmutable('2023-01-01 09:00:00');
+        $endDate = new DateTimeImmutable('2023-01-01 11:30:00');
+
+        $eventOccurrence = EventOccurrence::create(
+            $eventId,
+            $startDate,
+            $endDate,
+        );
+
+        $this->assertEquals($eventId, $eventOccurrence->id());
+        $this->assertEquals($startDate, $eventOccurrence->startDate());
+        $this->assertEquals($endDate, $eventOccurrence->endDate());
+    }
+
+    /**
+     * @test
+     */
+    public function it_should_calculate_the_duration_correctly(): void
+    {
+        $eventOccurrence = EventOccurrence::create(
+            new EventIdFake('1'),
+            new DateTimeImmutable('2023-01-01 09:00:00'),
+            new DateTimeImmutable('2023-01-01 11:30:00'),
+        );
+
+        $duration = DateInterval::createFromDateString('2 hours + 30 minutes');
+
+        $this->assertEquals($duration->format('d'), $eventOccurrence->duration()->format('d'));
+        $this->assertEquals($duration->format('H'), $eventOccurrence->duration()->format('H'));
+        $this->assertEquals($duration->format('i'), $eventOccurrence->duration()->format('i'));
+    }
+}
