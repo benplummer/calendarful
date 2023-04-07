a1596f3335ae9166adcdc592668caa05f1d3d465
Ben Plummer <me@benplummer.uk>
2023-04-07 10:17:49 +0100

    Initial EventCollection implementation.

---
+++ src/Event/EventCollection.php
@@ -0,0 +1,62 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Plummer\Calendarful\Event;
+
+use ArrayIterator;
+use Countable;
+use InvalidArgumentException;
+use IteratorAggregate;
+
+/**
+ * @implements IteratorAggregate<int, EventInterface>
+ */
+class EventCollection implements Countable, IteratorAggregate
+{
+    /**
+     * @param array<int, EventInterface> $events
+     */
+    private function __construct(
+        private readonly array $events
+    ) {
+        if (! $this->areAllInstancesOfEvents($events)) {
+            throw new InvalidArgumentException('All array items must be an instance of EventInterface.');
+        }
+    }
+
+    /**
+     * @param array<int, EventInterface> $events
+     */
+    public static function fromArray(array $events): self
+    {
+        return new self(
+            array_values($events)
+        );
+    }
+
+    public function count(): int
+    {
+        return count($this->events);
+    }
+
+    /**
+     * @return ArrayIterator<int, EventInterface>
+     */
+    public function getIterator(): ArrayIterator
+    {
+        return new ArrayIterator($this->events);
+    }
+
+    /**
+     * @param array<array-key, mixed> $items
+     */
+    private function areAllInstancesOfEvents(array $items): bool
+    {
+        $nonEventItems = array_filter($items, function (mixed $item) {
+            return ! $item instanceof EventInterface;
+        });
+
+        return ! $nonEventItems;
+    }
+}
