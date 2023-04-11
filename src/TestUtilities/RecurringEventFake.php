708a81760b1ae7421061c2d8a38e6d2656e8fef1
Ben Plummer <me@benplummer.uk>
2023-04-11 19:09:52 +0100

    RecurringEvent fake for use in relevant tests.

---
+++ src/TestUtilities/RecurringEventFake.php
@@ -0,0 +1,58 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Plummer\Calendarful\TestUtilities;
+
+use DateInterval;
+use DateTimeImmutable;
+use Plummer\Calendarful\Event\EventIdInterface;
+use Plummer\Calendarful\Event\Recurrence\RecurrenceType\RecurrenceTypeId;
+use Plummer\Calendarful\Event\Recurrence\RecurringEventInterface;
+
+class RecurringEventFake implements RecurringEventInterface
+{
+    public function __construct(
+        protected EventIdInterface $id,
+        protected DateTimeImmutable $startDate,
+        protected DateTimeImmutable $endDate,
+        protected RecurrenceTypeId $recurrenceTypeId,
+        protected ?DateTimeImmutable $recursUntil = null,
+    ) {
+        $this->id = $id;
+        $this->startDate = $startDate;
+        $this->endDate = $endDate;
+        $this->recurrenceTypeId = $recurrenceTypeId;
+        $this->recursUntil = $recursUntil;
+    }
+
+    public function id(): EventIdInterface
+    {
+        return $this->id;
+    }
+
+    public function startDate(): DateTimeImmutable
+    {
+        return $this->startDate;
+    }
+
+    public function endDate(): DateTimeImmutable
+    {
+        return $this->endDate;
+    }
+
+    public function duration(): DateInterval
+    {
+        return $this->startDate->diff($this->endDate);
+    }
+
+    public function recurrenceTypeId(): RecurrenceTypeId
+    {
+        return $this->recurrenceTypeId;
+    }
+
+    public function recursUntil(): ?DateTimeImmutable
+    {
+        return $this->recursUntil;
+    }
+}
