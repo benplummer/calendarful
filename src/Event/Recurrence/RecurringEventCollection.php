f2545f39e26435dfa8443d50326200d464633b1e
Ben Plummer <me@benplummer.uk>
2023-04-11 19:02:54 +0100

    Initial RecurringEventCollection.

---
+++ src/Event/Recurrence/RecurringEventCollection.php
@@ -0,0 +1,85 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Plummer\Calendarful\Event\Recurrence;
+
+use ArrayIterator;
+use Countable;
+use IteratorAggregate;
+use InvalidArgumentException;
+
+/**
+ * @implements IteratorAggregate<int, RecurringEventInterface>
+ */
+class RecurringEventCollection implements Countable, IteratorAggregate
+{
+    /**
+     * @param array<int, RecurringEventInterface> $recurringEvents
+     */
+    private function __construct(
+        private readonly array $recurringEvents,
+    ) {
+        if (! $this->areAllInstancesOfRecurringEvents($recurringEvents)) {
+            throw new InvalidArgumentException(
+                'All array items must be an instance of RecurringEventInterface.',
+            );
+        }
+    }
+
+    /**
+     * @param array<int, RecurringEventInterface> $recurringEvents
+     */
+    public static function fromArray(
+        array $recurringEvents,
+    ): self {
+        return new self(
+            array_values(
+                $recurringEvents,
+            ),
+        );
+    }
+
+    /**
+     * @param callable(RecurringEventInterface):bool $filterCallable
+     */
+    public function filter(
+        callable $filterCallable,
+    ): self {
+        return self::fromArray(
+            array_filter(
+                $this->recurringEvents,
+                $filterCallable,
+            )
+        );
+    }
+
+    public function count(): int
+    {
+        return count(
+            $this->recurringEvents,
+        );
+    }
+
+    /**
+     * @return ArrayIterator<int, RecurringEventInterface>
+     */
+    public function getIterator(): ArrayIterator
+    {
+        return new ArrayIterator(
+            $this->recurringEvents,
+        );
+    }
+
+    /**
+     * @param array<array-key, mixed> $items
+     */
+    private function areAllInstancesOfRecurringEvents(array $items): bool
+    {
+        $nonRecurringEventItems = array_filter($items, function (mixed $item) {
+            return ! $item instanceof RecurringEventInterface;
+        });
+
+        return ! $nonRecurringEventItems;
+    }
+}
