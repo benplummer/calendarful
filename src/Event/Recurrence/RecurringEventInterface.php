85a508cfa9d6e44a1380815a5b48a375d592aa01
Ben Plummer <me@benplummer.uk>
2023-04-11 16:09:07 +0100

    Initial RecurringEventInterface.

---
+++ src/Event/Recurrence/RecurringEventInterface.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Plummer\Calendarful\Event\Recurrence;
+
+use DateTimeImmutable;
+use Plummer\Calendarful\Event\EventInterface;
+use Plummer\Calendarful\Event\Recurrence\RecurrenceType\RecurrenceTypeId;
+
+interface RecurringEventInterface extends EventInterface
+{
+    /**
+     * Get the recurrence type id of the event.
+     */
+    public function recurrenceTypeId(): RecurrenceTypeId;
+
+    /**
+     * Get the date that the recurring event should recur until, if one has been set.
+     */
+    public function recursUntil(): ?DateTimeImmutable;
+}
