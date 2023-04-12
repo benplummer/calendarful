f8252571e2e2f2fb47c3a71365bf53521ef69544
Ben Plummer <me@benplummer.uk>
2023-04-12 08:56:23 +0100

    Initial RecurrenceTypeInterface.

---
+++ src/Event/Recurrence/RecurrenceType/RecurrenceTypeInterface.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Plummer\Calendarful\Event\Recurrence\RecurrenceType;
+
+use DateInterval;
+use DateTimeImmutable;
+use Plummer\Calendarful\Event\Recurrence\EventOccurrenceCollection;
+use Plummer\Calendarful\Event\Recurrence\RecurringEventCollection;
+
+interface RecurrenceTypeInterface
+{
+    /**
+     * Get the unique ID that will be referenced by relevant recurring events.
+     */
+    public function id(): RecurrenceTypeId;
+
+    /**
+     * Get a date interval that indicates the maximum limit of time that event occurrences
+     * for a given event can be generated up to.
+     */
+    public function recursUntilLimit(): DateInterval;
+
+    /**
+     * Generate the occurrences for the given recurring events.
+     */
+    public function occurrencesFor(
+        RecurringEventCollection $recurringEvents,
+        DateTimeImmutable $fromDate,
+        DateTimeImmutable $toDate
+    ): EventOccurrenceCollection;
+}
