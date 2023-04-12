ba2a5be18eb9ebd9c2fff0800138a5215b6d86f1
Ben Plummer <me@benplummer.uk>
2023-04-12 15:13:09 +0100

    Initial MonthlyDate recurrence type.

---
+++ src/Event/Recurrence/RecurrenceType/Type/MonthlyDate.php
@@ -0,0 +1,106 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Plummer\Calendarful\Event\Recurrence\RecurrenceType\Type;
+
+use DateInterval;
+use DatePeriod;
+use DateTimeImmutable;
+use Plummer\Calendarful\Event\Recurrence\EventOccurrence;
+use Plummer\Calendarful\Event\Recurrence\EventOccurrenceCollection;
+use Plummer\Calendarful\Event\Recurrence\RecurrenceType\RecurrenceTypeInterface;
+use Plummer\Calendarful\Event\Recurrence\RecurrenceType\RecurrenceTypeId;
+use Plummer\Calendarful\Event\Recurrence\RecurringEventCollection;
+use Plummer\Calendarful\Event\Recurrence\RecurringEventInterface;
+
+class MonthlyDate implements RecurrenceTypeInterface
+{
+    /**
+     * Get the unique ID that will be referenced by relevant recurring events.
+     */
+    public function id(): RecurrenceTypeId
+    {
+        return RecurrenceTypeId::fromString('MonthlyDate');
+    }
+
+    /**
+     * Get a date interval that indicates the maximum limit of time that event occurrences
+     * for a given event can be generated up to.
+     *
+     * @return DateInterval
+     * @abstract
+     */
+    public function recursUntilLimit(): DateInterval
+    {
+        return new DateInterval('P25Y');
+    }
+
+    /**
+     * Generate the occurrences for a given recurring event.
+     */
+    public function occurrencesFor(
+        RecurringEventCollection $recurringEvents,
+        DateTimeImmutable $fromDate,
+        DateTimeImmutable $toDate,
+    ): EventOccurrenceCollection {
+        $occurrences = [];
+
+        $monthlyDateRecurringEvents = $recurringEvents->filter(
+            function (RecurringEventInterface $recurringEvent) {
+                return $recurringEvent->recurrenceTypeId()->equals($this->id());
+            }
+        );
+
+        $startOfMonthForFromDate = new DateTimeImmutable($fromDate->format('Y-m-01 H:i:s'));
+
+        foreach ($monthlyDateRecurringEvents as $monthlyDateRecurringEvent) {
+            $recurringEventStartDate = $monthlyDateRecurringEvent->startDate();
+            $recurringEventStartTime = $recurringEventStartDate->format('H:i:s');
+
+            $monthlyDate = $recurringEventStartDate->format('d');
+            $startOfMonthForRecurringEvent = new DateTimeImmutable($recurringEventStartDate->format('Y-m-01 H:i:s'));
+
+            $recurringEventRecursUntil = $monthlyDateRecurringEvent->recursUntil();
+
+            $datePeriodStart = max($startOfMonthForFromDate, $startOfMonthForRecurringEvent);
+
+            $latestDatePeriodEnd = $datePeriodStart->add($this->recursUntilLimit());
+
+            $datePeriodEnd = $recurringEventRecursUntil
+                ? min($recurringEventRecursUntil, $toDate, $latestDatePeriodEnd)
+                : min($toDate, $latestDatePeriodEnd);
+
+            $datePeriod = new DatePeriod(
+                $datePeriodStart,
+                new DateInterval('P1M'),
+                $datePeriodEnd,
+                DatePeriod::INCLUDE_END_DATE,
+            );
+
+            foreach ($datePeriod as $date) {
+                if ($monthlyDate > $date->format('t')) {
+                    continue;
+                }
+
+                $occurrenceStartDate = new DateTimeImmutable(
+                    $date->format('Y-m-' . $monthlyDate) . ' ' . $recurringEventStartTime
+                );
+
+                if ($occurrenceStartDate < $datePeriodStart) {
+                    continue;
+                }
+
+                $occurrences[] = EventOccurrence::create(
+                    $monthlyDateRecurringEvent->id(),
+                    $occurrenceStartDate,
+                    $occurrenceStartDate->add($monthlyDateRecurringEvent->duration()),
+                );
+            }
+        }
+
+        return EventOccurrenceCollection::fromArray(
+            $occurrences,
+        );
+    }
+}
