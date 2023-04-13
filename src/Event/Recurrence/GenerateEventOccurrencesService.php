66e724cabe0b9fa04922e13395622f205cbd02ed
Ben Plummer <me@benplummer.uk>
2023-04-13 08:52:07 +0100

    Initial GenerateEventOccurrencesService.

---
+++ src/Event/Recurrence/GenerateEventOccurrencesService.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Plummer\Calendarful\Event\Recurrence;
+
+use DateTimeImmutable;
+use Plummer\Calendarful\Event\Recurrence\RecurrenceType\RecurrenceTypeRepositoryInterface;
+
+class GenerateEventOccurrencesService
+{
+    public function __construct(
+        protected RecurrenceTypeRepositoryInterface $recurrenceTypeRepository,
+    ) {
+    }
+
+    public function execute(
+        RecurringEventCollection $recurringEvents,
+        DateTimeImmutable $fromDate,
+        DateTimeImmutable $toDate,
+    ): EventOccurrenceCollection {
+        $eventOccurrences = EventOccurrenceCollection::fromArray(
+            [],
+        );
+
+        foreach ($this->recurrenceTypeRepository->all() as $recurrenceType) {
+            $generatedOccurrences = $recurrenceType->occurrencesFor(
+                $recurringEvents,
+                $fromDate,
+                $toDate,
+            );
+
+            $eventOccurrences = $eventOccurrences->merge(
+                $generatedOccurrences,
+            );
+        }
+
+        return $eventOccurrences;
+    }
+}
