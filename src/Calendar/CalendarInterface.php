1840124f89bb6cdcac574da6239bf65d11e5456f
Ben Plummer <me@benplummer.uk>
2023-05-08 12:56:15 +0100

    Leave Calendar empty for now.

--- src/Calendar/CalendarInterface.php
+++ src/Calendar/CalendarInterface.php
@@ -1,36 +1,9 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Plummer\Calendarful\Calendar;
 
-use Plummer\Calendarful\Event\EventRegistryInterface;
-
-/**
- * Interface CalendarInterface
- *
- * A common interface for calendars to use.
- *
- * @package Plummer\Calendarful
- * @abstract
- */
-interface CalendarInterface extends \Countable, \IteratorAggregate
+interface CalendarInterface
 {
-    /**
-     * Populate the calendar with events persisted from the event registry.
-     *
-     * @param  EventRegistryInterface   $eventsRegistry
-     * @param  \DateTime                $fromDate
-     * @param  \DateTime                $toDate
-     * @param  int                      $limit
-     * @param  array                    $extraFilters
-     * @return static
-     * @abstract
-     */
-    public function populate(EventRegistryInterface $eventsRegistry, \DateTime $fromDate, \DateTime $toDate, $limit = null, array $extraFilters = array());
-
-    /**
-     * Sort the events that the calendar contains.
-     *
-     * @return static
-     */
-    public function sort();
 }
